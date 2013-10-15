f31557c271a919d23a0f410d3292ac05a29d310a
root <root@mailserver01.bastelfreak.org>
2013-10-15 17:54:14 +0200

    initial commit

---
+++ setup_libvirt.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##
+# written by Tim 'bastelfreak' Meusel (https://bastelfreak.de)
+# compiles libvirt under debian/ubuntu
+# some more infos are available at my blog https://blog.bastelfreak.de/?p=659
+##
+
+aptitude install -y libvirt-bin libyajl-dev libxml2-dev libdevmapper-dev python-dev make > /dev/null
+mv /etc/libvirt/libvirtd.conf /etc/libvirt/libvirtd.conf.bak
+cd /usr/src
+wget ftp://libvirt.org/libvirt/libvirt-1.1.3.tar.gz
+tar xzf libvirt-1.1.3.tar.gz
+rm libvirt-1.1.3.tar
+cd libvirt-1.1.3
+./configure --prefix=/usr --with-storage-disk=no --without-selinux --with-qemu-group=libvirtd --with-qemu-user=libvirt-qemu --without-macvtap
+make -j8 && make install
+mv /etc/libvirt/libvirtd.conf.bak /etc/libvirt/libvirtd.conf
+service libvirt-bin restart
+
