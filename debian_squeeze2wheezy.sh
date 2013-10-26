d5fd333a60c0e081e7bff9ca937b51fb71fc2a6e
root <root@webserver01.bastelfreak.org>
2013-10-26 12:37:13 +0200

    Script for upgrading debian 6 to 7 (will maybe break with backports or third party repos)

---
+++ debian_squeeze2wheezy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+aptitude update
+aptitude safe-upgrade
+aptitude dist-upgrade
+sed -i 's/squeeze/wheezy/g' /etc/apt/sources.list
+aptitude update
+aptitude upgrade
+aptitude dist-upgrade
