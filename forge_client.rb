5f5370926f82c58d5c92a4c22d971a24b571f56f
Tim Meusel <tim@bastelfreak.de>
2014-09-12 18:19:00 +0200

    added basic get_requirements and comments

--- forge_client.rb
+++ forge_client.rb
@@ -19,85 +19,95 @@
 ##
 # todo: 
 # implement push upstream - done
-# implement logic
+# implement logiic - better
+# implement get_requirements - replace / with - and return a hash
 ##
 
 require 'puppet_forge'
 require 'git'
 require 'net/ssh'
 require 'yaml'
+class PuppetWrapper
+  PuppetForge.user_agent = 'bastelfreak was here'
+  @testmodule = 'lvm'
+  @path = '/home/bastelfreak/HE-puppet-admin-git'
+  @mngt_repo = 'gitolite-admin'
+  @mngt_repo_path = "#{path}/#{mngt_repo}"
+  @sshalias = 'gitolite-admin-node'
+  @host = 'master.puppet.local'
+  @user = 'git'
+  @key = '/home/bastelfreak/.ssh/id_rsa_git_admin'
 
-PuppetForge.user_agent = 'bastelfreak was here'
-testmodule = 'lvm'
-path = '/home/bastelfreak/HE-puppet-admin-git'
-mngt_repo = 'gitolite-admin'
-mngt_repo_path = "#{path}/#{mngt_repo}"
-sshalias = 'gitolite-admin-node'
-host = 'master.puppet.local'
-user = 'git'
-key = '/home/bastelfreak/.ssh/id_rsa_git_admin'
-
-# this function will do a git checkout
-def clone_and_move(res)
-  g = Git.clone res.homepage_url, res.name, :path => path
-  g.remote('origin').remove
-  g.add_remote 'origin', "#{sshalias}:#{res.name}"
-  g.push 'origin', 'master', :set_upstream => true
-end
-
-# checks if our gitolite already serves a suitable repo
-# otherwise create one
-def add_repo()
-  repos = get_current_repos
-  unless repos.include? res.name
-    mngt_g = Git.open mngt_repo_path
-    f = File.open "#{mngt_repo_path}/conf/gitolite.conf", 'a'
-    f.write "repo #{res.name}\n"
-    f.write "\tRW+ = @all\n"
-    f.close
-    mngt_g.commit_all "added repo #{res.name}"
-    mngt_g.push
+  # this method will do a git checkout
+  def clone_and_move(res)
+    g = Git.clone res.homepage_url, res.name, :path => path
+    g.remote('origin').remove
+    g.add_remote 'origin', "#{sshalias}:#{res.name}"
+    g.push 'origin', 'master', :set_upstream => true
   end
 
-end
+  # checks if our gitolite already serves a suitable repo
+  # otherwise create one
+  def add_repo()
+    repos = get_current_repos
+    unless repos.include? res.name
+      mngt_g = Git.open mngt_repo_path
+      f = File.open "#{mngt_repo_path}/conf/gitolite.conf", 'a'
+      f.write "repo #{res.name}\n"
+      f.write "\tRW+ = @all\n"
+      f.close
+      mngt_g.commit_all "added repo #{res.name}"
+      mngt_g.push
+    end
 
-# connects via ssh to our gitolite service and parses the output
-# ssh output:
-#hello bastelfreak, this is git@master running gitolite3 v3.6.1-6-gdc8b590 on git 1.8.3.1
-#
-# R W  gitolite-admin
-# R W  kickstack
-# R W  puppet-exportfact
-# R W  puppet-pwgen
-# R W  puppetlabs-concat
-# R W  puppetlabs-lvm
-# R W  puppetlabs-openstack
-# R W  puppetlabs-stdlib
-# R W  r10k-management
-# R W  testing
-def get_current_repos()
-  ary = []
-  Net::SSH.start(sshalias, user) do |ssh| #le wild bug occured. why do we have to specify the user?!
-    output = ssh.exec!('info')
   end
-  output = output.lines.to_a[2..-1].join
-  output.each_line do |line|
-    ary << line[/.*\t(.*)$/, 1]
+
+  # return a pretty list of all requierd modules
+  def get_requirements
+     unless release.metadata['dependencies'].empty?
+    #=> [{"name"=>"puppetlabs/stdlib", "version_requirement"=>"4.1.x"}]
+
   end
-end
 
-# wrapper function
-def voodoo
-  add_repo
-  clone_and_move
-end
+  # connects via ssh to our gitolite service and parses the output
+  # ssh output:
+  #hello bastelfreak, this is git@master running gitolite3 v3.6.1-6-gdc8b590 on git 1.8.3.1
+  #
+  # R W  gitolite-admin
+  # R W  kickstack
+  # R W  puppet-exportfact
+  # R W  puppet-pwgen
+  # R W  puppetlabs-concat
+  # R W  puppetlabs-lvm
+  # R W  puppetlabs-openstack
+  # R W  puppetlabs-stdlib
+  # R W  r10k-management
+  # R W  testing
+  def get_current_repos()
+    ary = []
+    Net::SSH.start(sshalias, user) do |ssh| #le wild bug occured. why do we have to specify the user?!
+      output = ssh.exec!('info')
+    end
+    output = output.lines.to_a[2..-1].join
+    output.each_line do |line|
+      ary << line[/.*\t(.*)$/, 1]
+    end
+  end
 
-result = PuppetForge::Module.where(query: testmodule).all
-if result.total == 1
-  res = result.first
-  voodoo res
-else
-  puts result.inspect
+  # wrapper function
+  def voodoo
+    # for main repo + each repo in $dependencies do
+    add_repo
+    clone_and_move
+  end
+
+  result = PuppetForge::Module.where(query: testmodule).all
+  if result.total == 1
+    res = result.first
+    voodoo res
+  else
+    puts result.inspect
+  end
 end
 
 # possible module names:
