66a2f4e47405114096a24a34233476d340ed0612
bastelfreak <tim@bastelfreak.de>
2015-02-22 12:49:33 +0100

    fix typo

--- tune_md.sh
+++ tune_md.sh
@@ -2,7 +2,7 @@
 
 ##
 # small snippet that increases the read and write cache of a md device
-# wirten by Tim 'bastelfreak' Meusel
+# written by Tim 'bastelfreak' Meusel
 # http://h3x.no/2011/07/09/tuning-ubuntu-mdadm-raid56
 ##
 echo 32768 > /sys/block/md4/md/stripe_cache_size
